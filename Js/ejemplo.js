a95f7f1826cc6acb29efe877a8d6a2101e14d402
Juan282007 <juanpablogomezperdomo@gmail.com>
2026-03-05 10:06:20 -0500

    Update

--- Js/ejemplo.js
+++ Js/ejemplo.js
@@ -1,4 +1,17 @@
-if (!localStorage.getItem("alertMostrado")) {
+if (!sessionStorage.getItem("inicioVisitado")) {
     alert("Bienvenido al Blog Educativo HTML");
-    localStorage.setItem("alertMostrado", "true");
+    sessionStorage.setItem("inicioVisitado", "true");
 }
+
+
+let menu = document.querySelectorAll(".menu");
+
+menu.forEach(opcion => {
+    opcion.addEventListener("mouseover", function(){
+        this.style.color = "#ADD8E6";
+    });
+
+    opcion.addEventListener("mouseout", function(){
+        this.style.color = "#ffffff";
+    });
+});
